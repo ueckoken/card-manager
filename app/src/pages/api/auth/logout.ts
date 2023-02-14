674a937f6b9f5cc1807de7e576aa1bd6651a6660
VOID <kouichi.hirachi@gmail.com>
2023-02-14 18:00:06 +0900

    fix bug

--- app/src/pages/api/auth/logout.ts
+++ app/src/pages/api/auth/logout.ts
@@ -1,5 +1,5 @@
 export default (req: any, res: any) => {
-  res.redirect(`${process.env.KEYCLOAK_ISSUER as string}/protocol/openid-connect/logout?redirect_uri=${encodeURIComponent(
+  res.redirect(`${process.env.KEYCLOAK_ISSUER as string}/protocol/openid-connect/logout?post_logout_redirect_uri=${encodeURIComponent(
     "https://card.ueckoken.club"
-  )}`);
+  )}&id_token_hint=${req.cookies.id_token}`);
 };
