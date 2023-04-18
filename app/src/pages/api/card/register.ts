1d4085c390bef371c6e1446b9bfb4cfb6c38331a
Koichi Hirachi <kouichi.hirachi@gmail.com>
2023-04-18 16:09:53 +0900

    update console

--- app/src/pages/api/card/register.ts
+++ app/src/pages/api/card/register.ts
@@ -11,7 +11,7 @@ export default async function handler(req: any, res: any) {
   // get token from header using nextauth
   console.log(req);
   const session = await getSession({ req });
-  console.log(session);
+  console.log("Session:", session);
   if (!session) {
     res.status(401).json({ error: 'Unauthorized' });
     return;
