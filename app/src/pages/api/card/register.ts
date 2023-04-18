5af05b8278e1b59ec93fc676dbe6ba608a5f552b
Koichi Hirachi <kouichi.hirachi@gmail.com>
2023-04-18 16:39:41 +0900

    force

--- app/src/pages/api/card/register.ts
+++ app/src/pages/api/card/register.ts
@@ -3,11 +3,11 @@ import { getSession } from 'next-auth/react';
 
 // get all cards using prisma
 export default async function handler(req: any, res: any) {
-  if (req.method !== 'POST') {
-    // return method not allowed
-    res.status(405).json({ error: 'Method not allowed' });
-    return
-  }
+  // if (req.method !== 'POST') {
+  //   // return method not allowed
+  //   res.status(405).json({ error: 'Method not allowed' });
+  //   return
+  // }
   // get token from header using nextauth
   console.log(req);
   const session = await getSession({ req });
